30773a158440dd87fa7afc7902d02b1484aac5ec
Bestydanis <Bmorales@ms223.org>
2016-09-29 14:27:48 -0400

    Add files via upload

---
+++ Bouncyball/sketch.js
@@ -0,0 +1,14 @@
+var xpos=1
+var ypos=1
+var xdir=1
+var ydir=1
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+
+}
+
+function draw() {
+  background("white");
+  if (xpos>=windowWidth || xpos<=0);
+  
+}
